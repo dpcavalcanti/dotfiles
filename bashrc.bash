4ca57048f7871806bcfad15f05bee0e238a3af01
Deni Cavalcanti <dpc@cesar.org.br>
2018-03-29 07:17:03 -0300

    Add new iterm profile and docker completion configuration

--- bashrc.bash
+++ bashrc.bash
@@ -9,8 +9,14 @@ alias brew-cask='brew cask'
 #Include $HOME/bin directory in the PATH
 [ -d $HOME/bin ] && export PATH=$HOME/bin:$PATH
 
+#Activate Docker commands completions
+[ -f /usr/local/etc/bash_completion.d/docker ] || ln -s ~/Applications/Docker.app/Contents/Resources/etc/docker.bash-completion /usr/local/etc/bash_completion.d/docker
+[ -f /usr/local/etc/bash_completion.d/docker-machine ] || ln -s ~/Applications/Docker.app/Contents/Resources/etc/docker-machine.bash-completion /usr/local/etc/bash_completion.d/docker-machine
+[ -f /usr/local/etc/bash_completion.d/docker-compose ] || ln -s ~/Applications/Docker.app/Contents/Resources/etc/docker-compose.bash-completion /usr/local/etc/bash_completion.d/docker-compose
+
 #Activate brew bash completion 
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+
 
 #add some handy aliases
 [ -f "${DOTFILES_DIR}/bash_aliases.bash" ] && source ${DOTFILES_DIR}/bash_aliases.bash
